67c6a332a392e2c7f3cd8901d0ba6b1d5078874e
sweetpanda68 <mgirma976@gmail.com>
2023-06-23 17:52:44 -0700

    This_manifest_creates_a_file_at_tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# This manifest creates a file at /tmp 
- file { '/tmp/school': 
-     ensure  => 'file', 
-     owner   => 'www-data', 
-     group   => 'www-data', 
-     mode    => '0744', 
-     content => 'I love Puppet', 
- }
+# This manifest creates a file at /tmp
+ file { '/tmp/school':
+     ensure  => 'file',
+     owner   => 'www-data',
+     group   => 'www-data',
+     mode    => '0744',
+     content => 'I love Puppet',
+     path    => '/tmp/school',
+}
