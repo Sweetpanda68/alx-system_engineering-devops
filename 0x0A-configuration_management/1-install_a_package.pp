a32a8db0bd893c457b64bdcec2ff72898f172aa2
sweetpanda68 <mgirma976@gmail.com>
2023-11-30 13:30:00 -0800

    configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Using Puppet, install flask from pip3
-package { 'flask':
+# using puppet, install flask from pip3
+package { 'flask' :
   ensure   => '2.1.0',
   name     => 'flask',
-  provider => 'pip3',
+  provider => 'pip3'
 }
